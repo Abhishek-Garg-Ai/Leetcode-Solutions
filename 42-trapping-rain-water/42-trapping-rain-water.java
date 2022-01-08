900e0dfd1b8b2d1ecc90a9c928a97c63fcf5d8eb
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-01-08 12:50:12 +0530

    Time: 2 ms (34.36%), Space: 42.1 MB (15.23%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,20 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        int[] right_max=new int[height.length];
+        int n=height.length;
+        int[] right_max=new int[n];
         int max=0;
-        for(int i=height.length-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             right_max[i]=max;
             if(height[i]>max) max=height[i];
         }
         int left_max=0;
         int total=0;
-        for(int i=0;i<height.length;i++){
+        for(int i=0;i<n;i++){
             int capacity=Math.min(left_max,right_max[i])-height[i];
             if(capacity>0) total+=capacity;
             if(left_max<height[i]) left_max=height[i];
         }
-        // System.out.println(Arrays.toString(right_max));
         return total;
-        
     }
 }
