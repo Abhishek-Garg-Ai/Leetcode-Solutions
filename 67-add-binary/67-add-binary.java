9e1b91b41fb01885d5d56529adae5920e4f7fadc
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-01-10 16:53:56 +0530

    Time: 2 ms (77.69%), Space: 39.3 MB (36.81%) - LeetHub

---
+++ 67-add-binary/67-add-binary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        char[] arr1=a.toCharArray();
+        char[] arr2=b.toCharArray();
+        StringBuilder sbr=new StringBuilder();
+        int bit1,bit2;
+        int carry=0;
+        int result;
+        int p1=arr1.length-1;
+        int p2=arr2.length-1;
+        while(p1>-1 || p2>-1 || carry==1){
+            bit1=(p1>-1)?Character.getNumericValue(arr1[p1--]):0;
+            bit2=(p2>-1)?Character.getNumericValue(arr2[p2--]):0;
+            result=bit1^bit2^carry;
+            carry=(bit1+bit2+carry>=2)?1:0;
+            sbr.append(result);
+        }
+        return sbr.reverse().toString();
+
+    }
+    
+}
