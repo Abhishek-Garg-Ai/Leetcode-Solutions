658b8227f9c20cacce50d0c98aeafe045b949374
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-01-10 16:55:24 +0530

    Time: 2 ms (77.69%), Space: 39.3 MB (36.81%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,16 +1,16 @@
 class Solution {
     public String addBinary(String a, String b) {
-        char[] arr1=a.toCharArray();
-        char[] arr2=b.toCharArray();
+        // char[] arr1=a.toCharArray();
+        // char[] arr2=b.toCharArray();
         StringBuilder sbr=new StringBuilder();
         int bit1,bit2;
         int carry=0;
         int result;
-        int p1=arr1.length-1;
-        int p2=arr2.length-1;
+        int p1=a.length()-1;
+        int p2=b.length()-1;
         while(p1>-1 || p2>-1 || carry==1){
-            bit1=(p1>-1)?Character.getNumericValue(arr1[p1--]):0;
-            bit2=(p2>-1)?Character.getNumericValue(arr2[p2--]):0;
+            bit1=(p1>-1)?Character.getNumericValue(a.charAt(p1--)):0;
+            bit2=(p2>-1)?Character.getNumericValue(b.charAt(p2--)):0;
             result=bit1^bit2^carry;
             carry=(bit1+bit2+carry>=2)?1:0;
             sbr.append(result);
