769357e8c17ca2f85daa112f9e6af38191c4ac5e
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-02-04 23:39:27 +0530

    Time: 2 ms (99.00%), Space: 42.5 MB (10.91%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,19 +1,15 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map=nge(nums2);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        Stack<Integer> st=new Stack<>();
+        int curr=0;
+        while(curr<nums2.length){
+            while(st.isEmpty()==false && nums2[curr]>st.peek()) map.put(st.pop(),nums2[curr]);
+            st.push(nums2[curr++]);
+        }
         int[] result=new int[nums1.length];
         for(int i=0;i<nums1.length;i++) result[i]=map.getOrDefault(nums1[i],-1);
         return result;
     }
     
-    public HashMap<Integer,Integer> nge(int[] arr){
-        HashMap<Integer,Integer> map=new HashMap<>();
-        Stack<Integer> st=new Stack<>();
-        int curr=0;
-        while(curr<arr.length){
-            while(st.isEmpty()==false && arr[curr]>arr[st.peek()]) map.put(arr[st.pop()],arr[curr]);
-            st.push(curr++);
-        }
-        return map;
-    }
 }
