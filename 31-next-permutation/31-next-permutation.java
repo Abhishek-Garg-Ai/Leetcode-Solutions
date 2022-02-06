0f488bae37dfa2e35e0de7dda19ec20954b6ea5c
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-02-06 13:07:00 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (12.94%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int curr=n-2;
+        while(curr>-1 && nums[curr]>=nums[curr+1]) curr--;
+        if(curr==-1){
+            reverse(nums,0);
+            return;
+        }
+        int temp=n-1;
+        while(temp>curr && nums[curr]>=nums[temp]) temp--;
+        // System.out.println(Arrays.toString(nums)+" "+curr+" "+temp);
+        swap(nums,curr,temp);
+        reverse(nums,curr+1);
+        
+        // System.out.println("Y");
+        // Collections.reverse(Arrays.asList(nums));
+
+    }
+    public void swap(int[] nums,int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+    
+    public void reverse(int[] nums,int start){
+        for(int i=0;i<(nums.length-start)/2;i++){
+            int temp=nums[start+i];
+            nums[start+i]=nums[nums.length-1-i];
+            nums[nums.length-1-i]=temp;
+        }
+    }
+}
