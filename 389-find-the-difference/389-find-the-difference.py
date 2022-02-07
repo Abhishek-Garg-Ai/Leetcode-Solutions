4b804228a31f99a7fa208bf8cbf43360232cff87
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-02-07 11:06:49 +0530

    Time: 36 ms (76.52%), Space: 14 MB (88.02%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ans=0;
+        for i in s:
+            ans=ans^ord(i)
+        for i in t:
+            ans=ans^ord(i)
+        # print(ans)
+        return chr(ans)
