3f7d4224775c146ae181a2fe9f916688e30e17e1
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-02-08 13:12:07 +0530

    Time: 1 ms (98.89%), Space: 39.7 MB (16.77%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,13 +1,14 @@
 class Solution {
     public int addDigits(int num) {
-        if(num<10) return num;
-        int temp=num;
-        int s=0;
-        while(temp>0){
-            s+=temp%10;
-            temp/=10;
+        while(num>9){
+            int s=0;
+            while(num>0){
+                s+=num%10;
+                num/=10;
+            }
+            num=s;
         }
-        return addDigits(s);
+        return num;
         
     }
 }
