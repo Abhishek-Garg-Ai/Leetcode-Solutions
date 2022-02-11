a9a2b087b946acef4bdd281fa50d4942057868e3
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-02-11 17:56:35 +0530

    Time: 32 ms (33.98%), Space: 45.3 MB (15.17%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -3,12 +3,12 @@ class Solution {
         if(s1.length()>s2.length()) return false;
         HashMap<Character,Integer> map1=new HashMap<>();
         HashMap<Character, Integer> map2=new HashMap<>();
-        for(Character ch:s1.toCharArray())
-            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        for(int i=0;i<s1.length();i++){
+            map1.put(s1.charAt(i),map1.getOrDefault(s1.charAt(i),0)+1);
+            map2.put(s2.charAt(i),map2.getOrDefault(s2.charAt(i),0)+1);
+        }
         int p1=0;
-        int p2=0;
-        while(p2<s1.length())       
-            map2.put(s2.charAt(p2),map2.getOrDefault(s2.charAt(p2++),0)+1);
+        int p2=s1.length();
         
         while(p2<s2.length()){
             if(map1.equals(map2)==true) return true;
