b5b7902f46c07dd4779f87747f190b26d27ff562
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-02-11 18:19:04 +0530

    Time: 10 ms (62.34%), Space: 43.1 MB (27.09%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length()) return false;
-        HashMap<Character,Integer> map1=new HashMap<>();
-        HashMap<Character, Integer> map2=new HashMap<>();
+        int[] map1=new int[26];
+        int[] map2=new int[26];
         for(int i=0;i<s1.length();i++){
-            map1.put(s1.charAt(i),map1.getOrDefault(s1.charAt(i),0)+1);
-            map2.put(s2.charAt(i),map2.getOrDefault(s2.charAt(i),0)+1);
+            map1[s1.charAt(i)-'a']+=1;
+            map2[s2.charAt(i)-'a']+=1;
         }
         int p1=0;
         int p2=s1.length();
         
         while(p2<s2.length()){
-            if(map1.equals(map2)==true) return true;
-            map2.put(s2.charAt(p1),map2.get(s2.charAt(p1))-1);
-            if(map2.get(s2.charAt(p1))<=0) map2.remove(s2.charAt(p1));
-            map2.put(s2.charAt(p2),map2.getOrDefault(s2.charAt(p2),0)+1);
-            p1+=1;
-            p2+=1;
+            if(Arrays.equals(map1,map2)) return true;
+            map2[s2.charAt(p1++)-'a']-=1;
+            map2[s2.charAt(p2++)-'a']+=1;
         }
-        if(map1.equals(map2)==true) return true;
+        if(Arrays.equals(map1,map2)==true) return true;
         return false;
     }
 }
