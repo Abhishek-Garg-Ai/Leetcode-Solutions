e575a2f1db070ffdcf6412cf68bc836bb97a26c2
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-03-28 11:34:30 +0530

    Time: 2 ms (80.91%), Space: 48.4 MB (26.50%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] countBits(int n) {
         int[] result=new int[n+1];
         for(int i=1;i<result.length;i++)
-            result[i]=result[i>>1]+i%2;
+            result[i]=result[i/2]+i%2;
         return result;
     }
 }
