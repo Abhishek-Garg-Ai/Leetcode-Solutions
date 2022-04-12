19f6ad6f431000af4bc100986894a3638a4003d9
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-12 13:37:59 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (23.79%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,32 +1,26 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
+        for(int i=0;i<board.length;i+=1){
+            for(int j=0;j<board[0].length;j+=1){
                 int count=live_neighbors(board,i,j);
-                if(board[i][j]==1){
-                    if(count!=3 && count!=4) board[i][j]=2;
-                }else{
-                    if(count==3) board[i][j]=-1;
-                }
+                if(board[i][j]==1 && count!=3 && count!=4) board[i][j]=2;
+                else if(board[i][j]==0 && count==3) board[i][j]=-1;
             }
         }
         
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
+        for(int i=0;i<board.length;i+=1){
+            for(int j=0;j<board[0].length;j+=1){
                 if(board[i][j]==2) board[i][j]=0;
                 else if(board[i][j]==-1) board[i][j]=1;
-                
             }
         }
-    
     }
     
     public int live_neighbors(int[][] board, int cr, int cc){
         int ans=0;
-        for(int i=Math.max(0,cr-1);i<=Math.min(board.length-1,cr+1);i++){
-            for(int j=Math.max(0,cc-1);j<=Math.min(board[0].length-1,cc+1);j++)
+        for(int i=Math.max(0,cr-1);i<=Math.min(board.length-1,cr+1);i+=1)
+            for(int j=Math.max(0,cc-1);j<=Math.min(board[0].length-1,cc+1);j+=1)
                 if(board[i][j]==1 || board[i][j]==2) ans+=1;
-        }
         return ans;
     }
 }
