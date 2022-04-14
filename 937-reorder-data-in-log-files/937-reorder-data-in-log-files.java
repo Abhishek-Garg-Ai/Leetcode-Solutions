51e10eb5187ed5093bd10fb3b91ca279591fc598
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-14 22:52:50 +0530

    Time: 4 ms (92.24%), Space: 43.1 MB (82.48%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        String[] result=new String[logs.length];
+        PriorityQueue<String> pq=new PriorityQueue<>(new Comparator<String>(){
+            public int compare(String s1, String s2){
+                String[] arr1=s1.split(" ",2);
+                String[] arr2=s2.split(" ",2);
+                int res=arr1[1].compareTo(arr2[1]);
+                if(res!=0) return res;
+                return arr1[0].compareTo(arr2[0]);
+            }
+        });
+        int curr=logs.length-1;
+        for(int i=logs.length-1;i>-1;i--){
+            if(Character.isAlphabetic(logs[i].charAt(logs[i].length()-1))) pq.add(logs[i]);
+            else result[curr--]=logs[i];
+        }
+        // pq.add(logs[1]);
+        // pq.add(logs[3]);
+        // pq.add(logs[4]);
+        // // System.out.println(pq);
+        curr=0;
+        while(pq.size()>0) result[curr++]=(pq.poll());
+        return result;
+    }
+}
