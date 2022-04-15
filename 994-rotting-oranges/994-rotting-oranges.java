80db7cd1991569344ab0591a8b8c4f70edb459ad
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-15 20:31:20 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (86.76%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -9,11 +9,6 @@ class Solution {
             }
         }
         if(ans>=101) return -1;
-        // for(int i=0;i<grid.length;i++){
-        //     for(int j=0;j<grid[0].length;j++){
-        //         if(grid[i][j]==1) return -1;
-        //     }
-        // }
         return ans;
     }
     
