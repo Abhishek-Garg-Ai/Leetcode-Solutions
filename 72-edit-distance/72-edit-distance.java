5a719a12dadb78dd83bb480b70ec74df71c9f7dc
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-18 08:57:09 +0530

    Time: 2 ms (100.00%), Space: 42.5 MB (82.09%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,21 +1,23 @@
 class Solution {
     int[][] dp;
     public int minDistance(String word1, String word2) {
-        dp=new int[word1.length()][word2.length()];
-        for(int i=0;i<word1.length();i++) Arrays.fill(dp[i],-1);
-        return helper(word1,word2,0,0);
+        char[] arr1=word1.toCharArray();
+        char[] arr2=word2.toCharArray();
+        dp=new int[arr1.length][arr2.length];
+        for(int i=0;i<arr1.length;i++) Arrays.fill(dp[i],-1);
+        return helper(arr1,arr2,0,0);
     }
     
-    public int helper(String word1,String word2,int c1,int c2){
-        if(c1==word1.length() && c2==word2.length()) return 0;
-        else if(c1==word1.length()) return (word2.length()-c2);
-        else if(c2==word2.length()) return (word1.length()-c1);
+    public int helper(char[] arr1,char[] arr2,int c1,int c2){
+        if(c1==arr1.length && c2==arr2.length) return 0;
+        else if(c1==arr1.length) return (arr2.length-c2);
+        else if(c2==arr2.length) return (arr1.length-c1);
         else if(dp[c1][c2]!=-1) return dp[c1][c2];
-        else if(word1.charAt(c1)==word2.charAt(c2)) 
-            return dp[c1][c2]=helper(word1,word2,c1+1,c2+1);
-        int m= helper(word1,word2,c1+1,c2);//delete
-        m=Math.min(m,helper(word1,word2,c1,c2+1));//insert
-        m=Math.min(m,helper(word1,word2,c1+1,c2+1));//replace
+        else if(arr1[c1]==arr2[c2]) 
+            return dp[c1][c2]=helper(arr1,arr2,c1+1,c2+1);
+        int m= helper(arr1,arr2,c1+1,c2);//delete
+        m=Math.min(m,helper(arr1,arr2,c1,c2+1));//insert
+        m=Math.min(m,helper(arr1,arr2,c1+1,c2+1));//replace
         return dp[c1][c2]=m+1;
     }
 }
