eef61d10cdd72612ab36dcaef75846aad6b90ce7
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-20 12:52:36 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (90.80%) - LeetHub

---
+++ 877-stone-game/877-stone-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // int alice=0;
+        // int bob=0;
+        // int p1=0;
+        // int p2=piles.length-1;
+        // while(p1<p2){
+        //     if(piles[p1]>piles[p2]){
+        //         alice+=
+        //     }    
+        // }
+        return true;
+    }
+    
+//     public int helper(int[] piles){
+        
+//     }
+}
