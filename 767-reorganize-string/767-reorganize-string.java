7301aa018a15ab26a654df486c6f5ee566e32438
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-24 11:03:47 +0530

    Time: 9 ms (22.98%), Space: 41.8 MB (70.47%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -4,13 +4,16 @@ class Solution {
         for(char ch:s.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
         PriorityQueue<Character> pq=new PriorityQueue<>((c1,c2)->map.get(c2)-map.get(c1));
         for(char ch:map.keySet()) pq.add(ch);
-        StringBuffer sb=new StringBuffer();
+        if(map.get(pq.peek())>(s.length()+1)/2) return "";
+        
+        char[] sb=new char[s.length()];
+        int indx=0;
         while(pq.size()>0){
             char ch1=pq.poll();
             int count_ch1=map.get(ch1);
             map.put(ch1,count_ch1-1);
             if(count_ch1==1) map.remove(ch1);
-            sb.append(ch1);
+            sb[indx++]=ch1;
             if(pq.size()==0 && count_ch1>1) return "";
             if(pq.size()>0){
                 char ch2=pq.poll();
@@ -19,9 +22,9 @@ class Solution {
                 map.put(ch2,count_ch2-1);
                 if(count_ch2==1) map.remove(ch2);
                 else pq.add(ch2);
-                sb.append(ch2);
+                sb[indx++]=ch2;
             }
         }
-        return sb.toString();    
+        return String.valueOf(sb);    
     }
 }
