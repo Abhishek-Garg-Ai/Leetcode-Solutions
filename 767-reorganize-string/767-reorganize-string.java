9e3db39d55321265550e071e725989cf941df0f6
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-24 11:04:18 +0530

    Time: 4 ms (65.59%), Space: 40.1 MB (97.55%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -5,7 +5,6 @@ class Solution {
         PriorityQueue<Character> pq=new PriorityQueue<>((c1,c2)->map.get(c2)-map.get(c1));
         for(char ch:map.keySet()) pq.add(ch);
         if(map.get(pq.peek())>(s.length()+1)/2) return "";
-        
         char[] sb=new char[s.length()];
         int indx=0;
         while(pq.size()>0){
@@ -14,7 +13,6 @@ class Solution {
             map.put(ch1,count_ch1-1);
             if(count_ch1==1) map.remove(ch1);
             sb[indx++]=ch1;
-            if(pq.size()==0 && count_ch1>1) return "";
             if(pq.size()>0){
                 char ch2=pq.poll();
                 int count_ch2=map.get(ch2);
