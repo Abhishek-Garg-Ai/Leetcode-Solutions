c5315cd0fc6c469e47e3bf7eaa38196363cb4477
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-04-25 11:01:31 +0530

    Time: 427 ms (27.41%), Space: 54.2 MB (40.00%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        char[] arr=s.toCharArray();
+        HashMap<Character,int[]> map=new HashMap<>();
+        map.put(arr[0],new int[]{0,0});
+        for(int i=1;i<s.length();i++){
+            if(map.containsKey(arr[i])){
+                map.get(arr[i])[1]=i;
+            }else{
+                map.put(arr[i],new int[]{i,i});
+            }
+        }
+        // for(char ch:map.keySet()){
+        //     System.out.println(ch+" "+Arrays.toString(map.get(ch)));
+        // }
+        HashSet<Character> ch_set;
+        int ans=0;
+        for(char ch:map.keySet()){
+            int[] loc=map.get(ch);
+            ch_set=new HashSet<>();
+            for(int i=loc[0]+1;i<loc[1];i++){
+                if(!ch_set.contains(arr[i])) ans+=1;
+                ch_set.add(arr[i]);
+            }
+        }
+        // System.out.println(map);
+        return ans;
+    }
+}
