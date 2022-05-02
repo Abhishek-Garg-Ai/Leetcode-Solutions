83863950554ef33c32bfc47ddfc7f141c8f4597e
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-02 14:43:48 +0530

    Time: 2 ms (87.38%), Space: 43.8 MB (97.24%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -2,18 +2,18 @@ class Solution {
     List<List<Integer>> result;
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         result=new ArrayList<>();
-        ArrayList<Integer> lt=new ArrayList<Integer>();
+        Deque<Integer> lt=new ArrayDeque<>();
         lt.add(0);
         helper(graph,0,lt);
         return result;
     }
     
-    public void helper(int[][] graph,int curr, ArrayList<Integer> lt){
+    public void helper(int[][] graph,int curr, Deque<Integer> lt){
         if(curr==graph.length-1) result.add(new ArrayList<Integer>(lt));
         for(int i=0;i<graph[curr].length;i++){
-                lt.add(graph[curr][i]);
+                lt.addLast(graph[curr][i]);
                 helper(graph,graph[curr][i],lt);
-                lt.remove(lt.size()-1);
+                lt.pollLast();
         }
     }
 }
