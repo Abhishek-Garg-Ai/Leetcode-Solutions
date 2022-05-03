7f5689333ee2afe1d123531cab06d053ac9e2aa5
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-03 22:14:01 +0530

    Time: 1 ms (98.93%), Space: 53.8 MB (58.62%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,20 +1,31 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        Stack<Integer> st=new Stack<>();
-        int min_i=nums.length;
-        for(int i=0;i<nums.length;i++){
-            while(st.size()>0 && nums[st.peek()]>nums[i])
-                min_i=Math.min(min_i,st.pop());
-            st.push(i);
+        int i=1;
+        // move i until we get our first unsorted elememt(from left)
+        while(i<nums.length && nums[i]>=nums[i-1]) i+=1;
+        if(i==nums.length) return 0;
+        int min=nums[i];
+        for(int j=i+1;j<nums.length;j++) min=Math.min(min,nums[j]);
+        for(int j=0;j<i;j++){
+            if(nums[j]>min){
+                min=j;
+                break;
+            }
         }
-        st.clear();
-        int max_i=0;
-        for(int i=nums.length-1;i>-1;i--){
-            while(st.size()>0 && nums[st.peek()]<nums[i])
-                max_i=Math.max(max_i,st.pop());
-            st.push(i);
+        // move i until we get our first unsorted elememt(from right)
+        i=nums.length-2;
+        while(i>-1 && nums[i]<=nums[i+1]) i-=1;
+        int max=nums[i];
+        for(int j=0;j<i;j++) max=Math.max(max,nums[j]);
+        //min: minimum value from the unsorted array
+        //max: maximum value from the unsorted array
+        for(int j=nums.length-1;j>i;j--){
+            if(nums[j]<max){
+                max=j;
+                break;
+            }
         }
-        // System.out.println(min_i+" "+max_i);
-        return max_i-min_i>0?max_i-min_i+1:0;
+        // System.out.println(min+" "+max);
+        return max-min+1;
     }
 }
