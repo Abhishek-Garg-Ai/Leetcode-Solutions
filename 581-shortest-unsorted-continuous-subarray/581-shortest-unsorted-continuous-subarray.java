0cdbff50f6c50efc2051e460b9461385011d4ea5
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-03 22:21:02 +0530

    Time: 2 ms (74.52%), Space: 54.2 MB (42.45%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,31 +1,35 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
+        // first we try to find leftmost element which is unsorted
+        // then we find the minimum element from the right of unsorted element.
+        // find the correct position in the sorted part for the minimum element
+        // now we know the correct starting index of the unsorted array.
         int i=1;
-        // move i until we get our first unsorted elememt(from left)
         while(i<nums.length && nums[i]>=nums[i-1]) i+=1;
         if(i==nums.length) return 0;
         int min=nums[i];
         for(int j=i+1;j<nums.length;j++) min=Math.min(min,nums[j]);
         for(int j=0;j<i;j++){
             if(nums[j]>min){
-                min=j;
+                min=j;// start
                 break;
             }
         }
-        // move i until we get our first unsorted elememt(from right)
+        // Now, we find rightmost element which is unsorted(reverse traversal)
+        // search for the max element from the left of unsorted element.
+        // find the correct position in the sorted part for the max element
+        // we get the correct ending index of the unsorted array.
         i=nums.length-2;
         while(i>-1 && nums[i]<=nums[i+1]) i-=1;
         int max=nums[i];
         for(int j=0;j<i;j++) max=Math.max(max,nums[j]);
-        //min: minimum value from the unsorted array
-        //max: maximum value from the unsorted array
         for(int j=nums.length-1;j>i;j--){
             if(nums[j]<max){
-                max=j;
+                max=j;//end
                 break;
             }
         }
-        // System.out.println(min+" "+max);
+        // length of the unsorted array will be start-end+1
         return max-min+1;
     }
 }
