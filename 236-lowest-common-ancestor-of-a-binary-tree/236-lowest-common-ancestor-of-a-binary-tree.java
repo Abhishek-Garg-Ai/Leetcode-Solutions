13b4b912ff0f13254a305cdd72a4af8181fbbc58
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-05 22:45:26 +0530

    Time: 1142 ms (5.08%), Space: 47.7 MB (28.94%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,20 +8,22 @@
  * }
  */
 class Solution {
+    // HashMap<TreeNode, Boolean> map=new HashMap<>();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==p || root==q) return root;
-        boolean has_p=helper(root.left,p.val);
-        boolean has_q=helper(root.left,q.val);
-        if(has_p && has_q)
+        // if(map.containsKey(root.))
+        int has_p=helper(root.left,p.val);
+        int has_q=helper(root.left,q.val);
+        if(has_p+has_q==2)
             return lowestCommonAncestor(root.left,p,q);
-        if(has_p || has_q)
+        if(has_p+has_q==1)
             return root;
         return lowestCommonAncestor(root.right,p,q);
     }
     
-    public boolean helper(TreeNode root, int p){
-        if(root==null) return false;
-        if(root.val==p) return true;
-        return helper(root.left,p) || helper(root.right,p);
+    public int helper(TreeNode root, int p){
+        if(root==null) return 0;
+        if(root.val==p) return 1;
+        return helper(root.left,p) + helper(root.right,p);
     }
 }
