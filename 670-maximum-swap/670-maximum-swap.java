b1ba49c8705702d86ad161bc535a0e22e9e0380d
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-09 15:19:57 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (86.05%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.java
+++ 670-maximum-swap/670-maximum-swap.java
@@ -1,20 +1,23 @@
 class Solution {
     public int maximumSwap(int num) {
-        String str=String.valueOf(num);
+        char[] str=String.valueOf(num).toCharArray();
         int i=1;
-        while(i<str.length() && str.charAt(i)<=str.charAt(i-1)) i+=1;
-        if(i==str.length()) return num;
+        while(i<str.length && str[i]<=str[i-1]) i+=1;
+        if(i==str.length) return num;
         int j=i-1;
         int m=i;
-        while(i<str.length()) {
-            if(str.charAt(i) >= str.charAt(m))
+        while(i<str.length) {
+            if(str[i] >= str[m])
                 m = i;
             i++;
         }
-        while(j>=0 && str.charAt(j)<str.charAt(m)) j-=1;
+        while(j>=0 && str[j]<str[m]) j-=1;
         j+=1;
-        String rets = str.substring(0, j) + str.charAt(m) + str.substring(j+1, m) + str.charAt(j) + str.substring(m+1);
-        return Integer.parseInt(rets);
+        // String rets = str.substring(0, j) + str.charAt(m) + str.substring(j+1, m) + str.charAt(j) + str.substring(m+1);
+        char temp=str[j];
+        str[j]=str[m];
+        str[m]=temp;
+        return Integer.parseInt(String.valueOf(str));
             
     }
 }
