6720f6d73ad25235b85baafd7b0baa19bd4c594c
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-09 11:08:26 +0530

    Time: 8 ms (72.56%), Space: 55.1 MB (57.91%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -1,24 +1,50 @@
 class Solution {
-    // boolean[] visited;
-    HashSet<Integer> set;
-    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        // visited=new boolean[n+1];
-        set=new HashSet<>();
-        boolean flag=true;
-        for(int i=0;i<n;i++){
-            if(helper(i,leftChild,rightChild) && set.size()==n) return true;
-            set.clear();
+    int[] p;
+    public boolean validateBinaryTreeNodes(int n, int[] left, int[] right) {
+        int[] parent=new int[n];
+        p = new int[n];
+        for(int i = 0; i < p.length; i++)
+            p[i] = i;
+        Arrays.fill(parent,-1);
+        for(int i=0;i<n;i++){ 
+            if(left[i] != -1) {
+                if(parent[left[i]] != -1) return false;
+                parent[left[i]]=i;
+            }
+            if(right[i] != -1) {
+                if(parent[right[i]] != -1) return false;
+                parent[right[i]]=i;
+            }
         }
-        return  false;
-            
-            
+        int c=0;
+        for(int i=0;i<n;i++) if(parent[i] == -1) c++;
+        if(c!=1) return false;
+        // System.out.println(Arrays.toString(parent));
+        int root = -1;
+        for(int i = 0; i < parent.length; i++) {
+            if(parent[i] == -1) {
+                root = i;
+                continue;
+            }
+            union(i, parent[i]);
+        }
+        for(int i = 0; i < n; i++)
+            find(i);
+        for(int i = 0; i < p.length; i++)
+            if(p[i] != root)
+                return false;
+        return true;
         
     }
     
-    public boolean helper(int curr,int[] left,int[] right){
-        if(curr==-1) return true;
-        if(set.contains(curr)) return false;
-        set.add(curr);
-        return helper(left[curr],left,right) && helper(right[curr],left,right);
+    
+    public void union(int a, int b) {
+        p[find(a)] = find(b);
+    }
+    
+    public int find(int a) {
+        if(p[a] == a)
+            return a;
+        return p[a] = find(p[a]);
     }
 }
