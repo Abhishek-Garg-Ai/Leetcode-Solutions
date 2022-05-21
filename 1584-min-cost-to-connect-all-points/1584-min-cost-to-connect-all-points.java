c9de22ed39b7096a9ed0a34eda934b9e5b9099fe
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-21 18:51:07 +0530

    Time: 394 ms (37.95%), Space: 184.5 MB (6.67%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -5,7 +5,7 @@ class Solution {
             map.put(i,points[i]);
         HashSet<Integer> set=new HashSet<>();
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)-> a[2]-b[2]);
-        pq.add(new int[]{0,0,0});
+        pq.offer(new int[]{0,0,0});
         int ans=0;
         while(!pq.isEmpty() && set.size()<points.length){
             int[] curr=pq.poll();
@@ -14,7 +14,7 @@ class Solution {
             set.add(curr[1]);
             for(int i=0;i<points.length;i++){
                 if(!set.contains(i))
-                pq.add(new int[]{curr[1],i,dist(map.get(curr[1]),map.get(i))});
+                pq.offer(new int[]{curr[1],i,dist(map.get(curr[1]),map.get(i))});
             }
         }
         return ans;
