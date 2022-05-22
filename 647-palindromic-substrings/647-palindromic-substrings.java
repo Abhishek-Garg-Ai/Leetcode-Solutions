27103d0f48005af31333dc158954b4f7d2ec192c
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-22 23:22:14 +0530

    Time: 32 ms (17.88%), Space: 51.3 MB (10.96%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,23 +1,24 @@
 class Solution {
-    // boolean[][] dp;
+    int[][] dp;
     public int countSubstrings(String s) {
         int n=s.length();
-        // dp=new boolean[n][n];
+        dp=new int[n][n];
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(helper(s,i,j)) ans+=1;
+                if(helper(s,i,j)==1) ans+=1;
             }
         }
         return ans;
         
     }
     
-    public boolean helper(String s,int i,int j){
-        if(i==j) return true; // single char
-        if(j==i+1) return s.charAt(i)==s.charAt(j); //double char
-        if(j<i) return false;
-        if(s.charAt(i)==s.charAt(j)) return helper(s,i+1,j-1);
-        return false;
+    public int helper(String s,int i,int j){
+        if(i==j) return 1; // single char
+        if(j==i+1) return s.charAt(i)==s.charAt(j)?1:-1; //double char
+        if(j<i) return -1;
+        if(dp[i][j]!=0) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)) return dp[i][j]=helper(s,i+1,j-1);
+        return -1;
     }
 }
