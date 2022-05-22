b5dae28cfcf27231b3f073c273c1ac70b187067f
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-22 18:08:16 +0530

    Time: 3 ms (42.38%), Space: 53.8 MB (55.21%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,23 +1,45 @@
 class Solution {
-    boolean[] visited;
-    public int findCircleNum(int[][] isConnected) {
-        visited=new boolean[isConnected.length];
-        int count=0;
-        for(int i=0;i<visited.length;i++){
-            if(!visited[i]){
-                dfs(isConnected,i);
-                count+=1;
+    int[] parent;
+    int[] rank;
+    public int findCircleNum(int[][] mat) {
+        int n=mat.length;
+        parent=new int[n];
+        // each node is parent of itself
+        for(int i=0;i<n;i++) parent[i]=i;
+        rank=new int[n];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1) union(i,j);
             }
         }
-        return count;
+        for(int i=0;i<n;i++) find(i);
+        
+        HashSet<Integer> s=new HashSet<>();
+        for(int repr:parent) s.add(repr);
+        return s.size();
     }
     
-    public void dfs(int[][] mat,int curr){
-        visited[curr]=true;
-        for(int i=0;i<mat.length;i++){
-            if(!visited[i] && mat[curr][i]==1){
-                dfs(mat,i);
-            }
-        }
+    public int find(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=find(parent[node]);// path compression
     }
+    
+    public void union(int u, int v){
+        int u_p=find(u);
+        int v_p=find(v);
+        if(u_p==v_p) return;
+        if(rank[u_p]<rank[v_p]) parent[u_p]=v_p;
+        if(rank[u_p]>rank[v_p]) parent[v_p]=u_p;
+        if(rank[u_p]==rank[v_p]){
+            parent[u_p]=v_p;
+            rank[v_p]+=1;
+        }
+        
+    }
+    
+    
+    
+    
+    
 }
