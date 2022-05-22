51181c531c237bbca577dc1f424aa671f0a165bb
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-22 18:27:37 +0530

    Time: 75 ms (12.50%), Space: 107.8 MB (12.50%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minimumLines(int[][] stock_price) {
+        if(stock_price.length==1) return 0;
         Arrays.sort(stock_price,(a,b)->a[0]-b[0]);
-        int ans=0;
-        double prev_slope=0.0;
-        int curr=1;
+        int ans=1;
+        double prev_slope=slope(stock_price[0],stock_price[1]);
+        int curr=2;
         while(curr<stock_price.length){
             double curr_slope=slope(stock_price[curr-1],stock_price[curr]);
-            // System.out.println(prev_slope+" "+curr_slope);
-            if(curr_slope!=prev_slope || (curr_slope==0 && ans==0)){
+            if(curr_slope!=prev_slope){
                 ans+=1;
                 prev_slope=curr_slope;
             }
