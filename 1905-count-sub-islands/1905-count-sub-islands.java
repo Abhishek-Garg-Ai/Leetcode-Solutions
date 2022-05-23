3644fc6b7b5f7412dd3d5c2d089214464872a18b
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-05-23 13:46:39 +0530

    Time: 46 ms (23.28%), Space: 140 MB (26.77%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,15 +1,17 @@
 class Solution {
     // int[] parent;
+    int[][] dp;
     int[][] directions=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         // parent=new int
         int ans=0;
+        dp=new int[grid1.length][grid1[0].length];
+        // for(int i=0;i< dp.length; i++) Arrays
         for(int i=0;i<grid1.length;i++){
             for(int j=0;j<grid1[0].length;j++){
                 if(grid2[i][j]==1){
-                    if(is_sub_island(grid1, grid2, i, j)){
+                    if(is_sub_island(grid1, grid2, i, j)==1){
                         ans+=1;
-                        // System.out.println(i+" "+j);
                     }
                 }
             }
@@ -17,19 +19,20 @@ class Solution {
         return ans;
     }
     
-    public boolean is_sub_island(int[][] grid1, int[][] grid2, int cr, int cc){
+    public int is_sub_island(int[][] grid1, int[][] grid2, int cr, int cc){
         if(cr<0 || cc<0 || cr>=grid1.length || cc>=grid1[0].length || grid2[cr][cc]==0)
-            return true;
-        if(grid1[cr][cc]==0) return false;
+            return 1;
+        if(grid1[cr][cc]==0) return -1;
+        if(dp[cr][cc]!=0) return dp[cr][cc];
         grid2[cr][cc]=0;
         grid1[cr][cc]=0;
         for(int[] dir: directions){
-            if(!is_sub_island(grid1,grid2, cr+dir[0],cc+dir[1])){
+            if(is_sub_island(grid1,grid2, cr+dir[0],cc+dir[1])==-1){
                 grid2[cr][cc]=1;
                 grid1[cr][cc]=1;
-                return false;
+                return dp[cr][cc]=-1;
             }
         }
-        return true;
+        return dp[cr][cc]=1;
     }
 }
