e4958c7f4901a09ebe06a62d347f4d3ecba4837d
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-10-16 19:30:43 +0530

    Time: 15 ms (66.01%), Space: 55.5 MB (22.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        // System.out.println(Arrays.deepToString(intervals));
         ArrayList<int[]> lt=new ArrayList<>();
         int[] curr=intervals[0];
-        int i=1;
-        while(i<intervals.length){
+        for(int i=0;i<intervals.length;i++){
             if(intervals[i][0]<=curr[1]) 
-                curr[1]=Math.max(curr[1],intervals[i++][1]);
+                curr[1]=Math.max(curr[1],intervals[i][1]);
             else{
                 lt.add(curr);
-                curr=intervals[i++];
+                curr=intervals[i];
             }
         }
         lt.add(curr);
-        // int[] arr = new int[lt.size()]; 
-        return lt.toArray(new int[0][]);
-        // return lt.toArray(arr);
-        // return intervals;
+        return lt.toArray(new int[lt.size()][]);
     }
 }
