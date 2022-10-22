0b73f6cb79a0c3994da542f77ede6f96fdc722ae
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-10-23 00:08:45 +0530

    Time: 1 ms (82.46%), Space: 43.7 MB (6.10%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -22,16 +22,8 @@ class Solution {
             }
             curr=curr.next;
         }
-        while(list1!=null){
-            curr.next=list1;
-            list1=list1.next;
-            curr=curr.next;
-        }
-        while(list2!=null){
-            curr.next=list2;
-            list2=list2.next;
-            curr=curr.next;
-        }
+        if(list1!=null) curr.next=list1;
+        if(list2!=null) curr.next=list2;
         return dummy.next;
     }
 }
