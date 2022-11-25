9e4e2e44aa565509b8111d033b31b742c67defc5
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-11-25 13:37:38 +0530

    Time: 17 ms (82.06%), Space: 45.8 MB (87.97%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -13,17 +13,5 @@ class Solution {
             }
         }
         return dp[text1.length()][text2.length()];
-        
-    }
-    
-    public int helper(String s1, String s2, int i1, int i2){
-        if(i1==s1.length() || i2==s2.length()) return 0;
-        if(dp[i1][i2]!=-1) return dp[i1][i2];
-        int ans=0;
-        if(s1.charAt(i1)==s2.charAt(i2))
-            ans=Math.max(ans,helper(s1,s2,i1+1,i2+1)+1);
-        ans=Math.max(ans,helper(s1,s2,i1+1,i2));
-        ans=Math.max(ans,helper(s1,s2,i1,i2+1));
-        return dp[i1][i2]=ans;
     }
 }
