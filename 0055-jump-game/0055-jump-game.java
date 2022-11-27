38fbdf084d88d79f6267da78fb5dd44ef969230f
Abhishek-Garg-Ai <67639150+Abhishek-Garg-Ai@users.noreply.github.com>
2022-11-27 13:24:26 +0530

    Time: 176 ms (21.23%), Space: 68.1 MB (45.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,10 +5,11 @@ class Solution {
         dp[0]=true;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0) continue;
-            for(int j=i+1;j<=Math.min(nums.length-1,i+nums[i])&&dp[i];j++)
+            for(int j=i+1;j<=Math.min(nums.length-1,i+nums[i])&&dp[i];j++){
+                if(j==nums.length-1) return true;
                 dp[j]=true;
+            }
         }
-        // System.out.println(Arrays.toString(dp));
         return dp[nums.length-1];
     }
     
